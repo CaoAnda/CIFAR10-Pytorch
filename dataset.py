c474be6cf19e40de057af5ec3ed6fa787189e76b
CaoAnda <1401650042@qq.com>
2022-11-04 11:06:54 +0000

    修改transform

--- dataset.py
+++ dataset.py
@@ -1,4 +1,3 @@
-import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 import torchvision
@@ -6,45 +5,42 @@ import torch
 from torchvision.transforms import transforms
 
 
-# 数据增广方法
-transform = transforms.Compose([
-    # +4填充至36x36
-    transforms.Pad(4),
-    # 随机水平翻转
-    transforms.RandomHorizontalFlip(), 
-    # 随机裁剪至32x32
-    transforms.RandomCrop(32), 
-    # 转换至Tensor
+test_transform = transforms.Compose([
     transforms.ToTensor(),
-    #  归一化
-#     transforms.Normalize(mean=(0.5, 0.5, 0.5),   # 3 for RGB channels
-#                                      std=(0.5, 0.5, 0.5))
+    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
 ])
-
+transform = transforms.Compose([
+    transforms.Pad(4),
+    transforms.RandomHorizontalFlip(), 
+    transforms.RandomCrop(32), 
+    test_transform
+])
 
 def get_dataset_loader(dataset_path, batch_size):
     train_dataset = torchvision.datasets.CIFAR10(
         root=dataset_path,
         train=True,
         transform=transform,
-        # download=True
+        # download=True,
     )
     test_dataset = torchvision.datasets.CIFAR10(
         root=dataset_path,
         train=False,
-        transform=transform,
+        transform=test_transform,
         # download=True
     )
 
     train_loader = torch.utils.data.DataLoader(
         dataset=train_dataset,
         batch_size=batch_size,
-        shuffle=True
+        shuffle=True,
+        num_workers=2
     )
     test_loader = torch.utils.data.DataLoader(
         dataset=test_dataset,
         batch_size=batch_size,
-        shuffle=False
+        shuffle=False,
+        num_workers=2
     )
 
     return train_loader, test_loader
